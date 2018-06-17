f2aada471767cc9db8fd00cec43a3db9ddb6fcd9
rawkush <ankushrawat950@gmail.com>
2018-06-17 20:46:55 +0530

    ListView used and using dummy data

--- app/src/main/java/com/example/ankush/eathquakereportapp/MainActivity.java
+++ app/src/main/java/com/example/ankush/eathquakereportapp/MainActivity.java
@@ -3,11 +3,26 @@ package com.example.ankush.eathquakereportapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //using temp dummy data
+        ArrayList<String> earthquakes= new ArrayList<>();
+        earthquakes.add("San Francisco");
+        earthquakes.add("London");
+        earthquakes.add("Tokyo");
+        earthquakes.add("Mexico City");
+        earthquakes.add("Moscow");
+        earthquakes.add("Rio de Janeiro");
+        earthquakes.add("Paris");
+
+
+
     }
 }
