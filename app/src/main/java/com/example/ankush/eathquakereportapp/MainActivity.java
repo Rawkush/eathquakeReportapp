f93bd7af579583af4872e6eff9d493faf5d0d996
rawkush <ankushrawat950@gmail.com>
2018-06-18 12:52:01 +0530

    adding asysnc atsask for networking

--- app/src/main/java/com/example/ankush/eathquakereportapp/MainActivity.java
+++ app/src/main/java/com/example/ankush/eathquakereportapp/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.ankush.eathquakereportapp;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String USGS_REQUEST_URL =
+            "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&orderby=time&minmag=6&limit=10";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +31,20 @@ public class MainActivity extends AppCompatActivity {
         EarthquakeAdapter adapter= new EarthquakeAdapter(this,earthquakes);
         earthquakeListView.setAdapter(adapter);
 
+    }
+
+
+
+    private class EarthquakeAsyncTask extends AsyncTask<String, Void, List<EarthquakeData>> {
+
+        @Override
+        protected List<EarthquakeData> doInBackground(String... urls) {
+
+        }
+
+        @Override
+        protected void onPostExecute(List<EarthquakeData> data) {
+
+        }
     }
 }
