6b96b97cf45f780f76a70c3d23111502f6875a57
rawkush <ankushrawat950@gmail.com>
2018-06-18 13:14:27 +0530

    updating files for networking

--- app/src/main/java/com/example/ankush/eathquakereportapp/MainActivity.java
+++ app/src/main/java/com/example/ankush/eathquakereportapp/MainActivity.java
@@ -39,14 +39,31 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected List<EarthquakeData> doInBackground(String... urls) {
+            // Don't perform the request if there are no URLs, or the first URL is null.
+            if (urls.length < 1 || urls[0] == null) {
+                return null;
+            }
 
+            List<EarthquakeData> result = QueryUtils.fetchEarthquakeData(urls[0]);
+            return result;
         }
+
 
         @Override
         protected void onPostExecute(List<EarthquakeData> data) {
 
         }
+
+        @Override
+        protected void onPostExecute(List<EarthquakeData> data) {
+            // Clear the adapter of previous earthquake data
+            adapter.clear();
+
+            // If there is a valid list of {@link Earthquake}s, then add them to the adapter's
+            // data set. This will trigger the ListView to update.
+            if (data != null && !data.isEmpty()) {
+                adapter.addAll(data);
+            }
+        }
     }
-
-
 }
